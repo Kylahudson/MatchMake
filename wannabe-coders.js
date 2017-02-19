2a6bf83df2c6bf539715beeb3f4acdca648669d9
Kylahudson <Kyla.hudson432@west-mec.org>
2017-02-19 00:35:30 -0700

    images and html pages

--- wannabe-coders.js
+++ wannabe-coders.js
@@ -1,8 +1,18 @@
+// var $ = jQuery
+//
+// $(function() {
+// 	$('#my-button').on('click', function() {
+// 		$('#my-form').show()
+// 		$('#my-form').html($('#some-element').html())
+// 	})
+// })
+
 var $ = jQuery
 
-$(function() {
-	$('#my-button').on('click', function() {
-		$('#my-form').show()
-		$('#my-form').html($('#some-element').html())
-	})
-})
+$(document).ready(function(){
+
+
+	$("p").on('click',function(){
+		$("this").show();
+	});
+});
